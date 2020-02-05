ad3cb1d68bae31114286f8d7cf1e7963d3e38a99
Christian júnior <christian_geajr@yahoo.com.br>
2020-02-04 21:38:29 -0300

    teste 1

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -22,6 +22,7 @@ namespace TrackerUI
             GlobalConfig.InitializeConnetcions(DatabaseType.Sql);
             //Application.Run(new TournamentDashboardForm()); // When this form is closed, the application will close even if there are other forms open.
             Application.Run(new TournamentDashboardForm());
+            //This was added to the branch teste
 
   
         }
@@ -33,7 +34,7 @@ namespace TrackerUI
 
         public static void TesteMetodo()
         {
-
+            //This was added to the branch teste
         }
     }
 }
